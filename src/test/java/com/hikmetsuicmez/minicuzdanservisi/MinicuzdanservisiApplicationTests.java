a2f9bcaa28ee529249404dc613167c527e419bd7
Hikmet Suiçmez <hikmetsuicmez@outlook.com>
2026-09-25 20:53:28 +0300

    Ticket 1 - Proje Iskeleti

---
+++ src/test/java/com/hikmetsuicmez/minicuzdanservisi/MinicuzdanservisiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikmetsuicmez.minicuzdanservisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinicuzdanservisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
